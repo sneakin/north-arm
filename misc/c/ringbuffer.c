c9ca98e29f37ad9b91a46b98cbd4935e38f1b355
Nolan Eakins <sneakin@semanticgap.com>
2022-05-06 04:51:13 -0400

    RingBuffer fix on signaling overflow and minimal test.

--- misc/c/ringbuffer.c
+++ misc/c/ringbuffer.c
@@ -7,9 +7,11 @@ void ring_buffer_init(RingBuffer *rb, char *buffer, int size) {
 }
 
 int ring_buffer_put(RingBuffer *rb, char c) {
+  int n = (1 + rb->wpos) % rb->size;
+  if(n == rb->rpos) return 0;
   rb->buffer[rb->wpos] = c;
-  rb->wpos = (1 + rb->wpos) % rb->size;
-  return rb->wpos != rb->rpos;
+  rb->wpos = n;
+  return n != rb->rpos;
 }
 
 int ring_buffer_get(RingBuffer *rb) {
@@ -22,3 +24,33 @@ int ring_buffer_get(RingBuffer *rb) {
   }
 }
 
+#ifdef TESTING
+#include <stdio.h>
+
+int main() {
+  RING_BUFFER(rb, 16);
+  int i, c;
+  for(i = 0; i < 4; i++) {
+    ring_buffer_put(&rb, i);
+  }
+  for(i = 0; i < 6; i++) {
+    c = ring_buffer_get(&rb);
+    printf("%i: %i\n", i, c);
+  }
+  for(i = 0; i < 20; i++) {
+    if(ring_buffer_put(&rb, i) == 0) {
+      printf("Filled: %i\n", rb.wpos);
+      break;
+    }
+    printf("%i %i\n", rb.wpos, rb.rpos);
+  }
+  for(i = 0; i < 20; i++) {
+    c = ring_buffer_get(&rb);
+    printf("%i: %i\n", i, c);
+  }
+  if(ring_buffer_put(&rb, 17) == 0) {
+    puts("Full");
+  }
+  return 0;
+}
+#endif
